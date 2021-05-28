3998a4599d6a7076905b413f398b3392448fc904
uhkwak <ukhyun91@gmail.com>
2021-05-28 16:43:59 +0900

    Update: Application 클래스 생성

---
+++ app/src/main/java/com/example/kakao_search/KakaoSearchApplication.kt
@@ -0,0 +1,8 @@
+package com.example.kakao_search
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+internal class KakaoSearchApplication : Application()
