acb3130be6eaa230ac758d0e39207e08f72b8456
uhkwak <ukhyun91@gmail.com>
2021-05-28 16:26:33 +0900

    Update: 기본 첫 화면으로 SearchFragment 로 지정

--- app/src/main/java/com/example/kakao_search/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/kakao_search/presentation/main/MainActivity.kt
@@ -2,12 +2,31 @@ package com.example.kakao_search.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.kakao_search.R
+import com.example.kakao_search.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        initialize()
+    }
+
+    private fun initialize() {
+        val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.container_fragment) as NavHostFragment? ?: return
+
+        navController = host.navController
     }
 }
