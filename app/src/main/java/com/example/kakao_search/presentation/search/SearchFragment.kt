2a963e6014551155b1517b7e52214a79f7f044ff
uhkwak <ukhyun91@gmail.com>
2021-05-29 13:36:43 +0900

    WIP: SearchViewMoel, SearchFragment 구성중

--- app/src/main/java/com/example/kakao_search/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/example/kakao_search/presentation/search/SearchFragment.kt
@@ -1,5 +1,26 @@
 package com.example.kakao_search.presentation.search
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 
-internal class SearchFragment {
+
+@AndroidEntryPoint
+internal class SearchFragment : Fragment() {
+
+    private val viewModel by viewModels<SearchViewModel>()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
 }
---
+++ app/src/main/java/com/example/kakao_search/presentation/search/SearchViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.kakao_search.presentation.search
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+
+@HiltViewModel
+internal class SearchViewModel : ViewModel() {
+
+}
